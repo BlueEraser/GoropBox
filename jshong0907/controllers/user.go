80667cbb712fc1db5f2d27a206b68fca8f3c5ea8
JoonShik <jshong0907@gmail.com>
2022-03-16 00:25:03 +0900

    FIX: signin to controller

--- jshong0907/controllers/user.go
+++ jshong0907/controllers/user.go
@@ -1,14 +1,16 @@
 package controllers
 
 import (
-	"net/http"
-
+	"github.com/golang-jwt/jwt"
+	"gorop-box/models"
 	"gorop-box/services"
+	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
-func Signup(c echo.Context) error {
+func SignUp(c echo.Context) error {
 	params := make(map[string]string)
 	c.Bind(&params)
 	user := services.CreateUser(
@@ -18,4 +20,38 @@ func Signup(c echo.Context) error {
 	)
 
 	return c.JSON(http.StatusOK, user)
+}
+
+func SignIn(c echo.Context) error {
+	params := make(map[string]string)
+	c.Bind(&params)
+	user, err := services.CheckPassword(params["email"], params["password"])
+
+	if err != nil {
+		return echo.ErrUnauthorized
+	}
+
+	jwtClaims := models.JwtClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+		user.Email,
+		user.NickName,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
+
+func GetUserInfo(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*models.JwtClaims)
+	return c.JSON(http.StatusOK, claims)
 }
--- jshong0907/main.go
+++ jshong0907/main.go
@@ -1,71 +1,26 @@
 package main
 
 import (
-	"net/http"
-	"time"
-
-	"gorop-box/controllers"
-	"gorop-box/services"
-
-	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorop-box/controllers"
+	"gorop-box/models"
 )
-
-type jwtClaims struct {
-	Email string `json:"email"`
-	jwt.StandardClaims
-}
 
 func main() {
 	e := echo.New()
-	e.POST("/signup", controllers.Signup)
-	e.POST("/signin", func(c echo.Context) error {
-		params := make(map[string]string)
-		c.Bind(&params)
-		user, err := services.CheckPassword(params["email"], params["password"])
-
-		if err != nil {
-			return echo.ErrUnauthorized
-		}
-
-		claims := jwtClaims{
-			user.NickName,
-			jwt.StandardClaims{
-				ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
-			},
-		}
-
-		// Create token with claims
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-		// Generate encoded token and send it as response.
-		t, err := token.SignedString([]byte("secret"))
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, echo.Map{
-			"token": t,
-		})
-	})
+	e.POST("/signup", controllers.SignUp)
+	e.POST("/signin", controllers.SignIn)
 
 	r := e.Group("/restricted")
 
 	// Configure middleware with the custom claims type
 	config := middleware.JWTConfig{
-		Claims:     &jwtClaims{},
+		Claims:     &models.JwtClaims{},
 		SigningKey: []byte("secret"),
 	}
 	r.Use(middleware.JWTWithConfig(config))
-	r.GET("", restricted)
+	r.GET("/user", controllers.GetUserInfo)
 
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func restricted(c echo.Context) error {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*jwtClaims)
-	email := claims.Email
-	return c.String(http.StatusOK, "Welcome "+email+"!")
 }
