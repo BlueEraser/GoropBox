89c45072f8be1659ef6cc158975a31376f78fcb8
phobyjun <phobyjun@gmail.com>
2022-03-23 23:34:18 +0900

    ADD: 유저 정보 조회 서비스 생성 (GetUserByEmail)

--- phobyjun/service/user.go
+++ phobyjun/service/user.go
@@ -16,7 +16,16 @@ func CreateUser(userDto *model.User) (*model.User, error) {
 		Email:    userDto.Email,
 	}
 	tx := db.Session.Create(&user)
-	if err = tx.Error; err != nil {
+	if err := tx.Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUserByEmail(email string) (*model.User, error) {
+	var user model.User
+	tx := db.Session.Where("email = ?", email).First(&user)
+	if err := tx.Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
