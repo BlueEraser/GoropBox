41ea22441f8497043eea1cc73bc6fdc2a2ba48fa
JoonShik <jshong0907@gmail.com>
2022-04-05 18:22:59 +0900

    CHG: router settings

--- jshong0907/python/users/urls.py
+++ jshong0907/python/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from users.views import SignupView, SigninView, UserView
 
 urlpatterns = [
-    path('signup/', SignupView.as_view()),
-    path('signin/', SigninView.as_view()),
-    path('test/', UserView.as_view()),
+    path('auth/signup/', SignupView.as_view()),
+    path('auth/signin/', SigninView.as_view()),
+    path('user/', UserView.as_view()),
 ]
